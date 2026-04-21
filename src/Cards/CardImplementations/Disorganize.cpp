efea5659284c90549795c192f008436764d812ef
PurpleCrowbar <26198472+PurpleCrowbar@users.noreply.github.com>
2026-04-21 18:06:22 +0100

    Style fix: `Game &game` -> `Game& game` in Disorganize source file

--- src/Cards/CardImplementations/Disorganize.cpp
+++ src/Cards/CardImplementations/Disorganize.cpp
@@ -1,6 +1,6 @@
 #include "Disorganize.h"
 
-void Disorganize::resolve(Game &game, PlayerID controller) {
+void Disorganize::resolve(Game& game, PlayerID controller) {
     game.performShuffle(controller);
     game.drawCards(controller, 1);
 }
